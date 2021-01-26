004b0e22d6f7f86660aa5a6b2b24e84f7da482de
Abdirizak-byte <59887662+Abdirizak-byte@users.noreply.github.com>
2021-01-26 15:51:21 -0800

    Add files via upload

---
+++ team1_itc134_githubpages/contact.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <title>Volunteer Computing</title>
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/big.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+    <!-- jQuery -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!-- MenuMaker Plugin -->
+    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js"></script>
+    <!-- Icon Library -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <script src="js/script.js"></script>
+ </head>
+ <body>
+     <!-- wrapper -->
+     <div class="wrapper">
+     <header>
+     <h1>Volunteer Computing</h1>
+     <nav id="cssmenu">
+  <ul>
+     <li><a href="index.html"><i class="fa fa-fw fa-home"></i> Home</a></li>
+     <li><a href="history.html"><i class="fa fa-fw fa-camera-retro"></i> History</a></li>
+     <!-- dropdown for research -->
+     <li><a href="#"> Research</a>
+        <ul>
+            <li><a href="smo.html"><i class="fa fa-fw fa-thumbs-o-up"></i> SMO</a></li>
+            <li><a href="accessibility.html"><i class="fa fa-fw fa-globe"></i> Accessibility</a></li>
+            <li><a href="future.html"><i class="fa fa-fw fa-globe"></i> Future</a></li>
+        </ul>
+     </li>
+    </ul>
+    </nav>
+</header>
+
+    <section class="fullwidth">
+        <h2 class="subheader">Contact</h2>
+        <p>Watch the supporting instructional video to create the contents for this page.</p>
+    </section>
+         <?php include 'includes/simple.php';?>
+	 <p class="clear-recaptcha"></p>
+    <footer>
+      <p><small>&copy; 2019 by <a href="contact.php">Contact Joon</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+  </div>
+     
+ </body>
+</html>
